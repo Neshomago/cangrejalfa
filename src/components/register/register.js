3d0cb3539916773ecee1cc1743320ef97bab5682
Andres Torres <magoyde@gmail.com>
2021-11-07 12:59:58 -0500

    register form, solo validacion y vista OK

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -18,104 +18,134 @@ const SignupSchema = Yup.object().shape({
 });
 
 class Register extends Component {
-  
-showForm = ({
-  values,
-  errors,
-  touched,
-  handleChange,
-  handleSubmit,
-  setFieldValue,
-  isSubmitting
-}) => {
-  return(
-    <form onSubmit={handleSubmit}>
-        <div class="form-group has-feedback">
+
+  showForm = ({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleSubmit,
+      setFieldValue,
+      isSubmitting
+    }) => {
+      return(
+        <form onSubmit={handleSubmit}>
+        <div className="form-group has-feedback">
+          <input 
+            type="text"
+            name="username"
+            placeholder="Username"
+            className="form-control"
+            onChange={handleChange}
+            value={values.username}
+            className={
+              errors.username && touched.username ? "form-control is-invalid" : "form-control"
+            }
+            />
+            {errors.fullname && touched.fullname ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.username}
+              </small>
+            ) : null}
+        </div>
+        <div className="form-group has-feedback">
           <input
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={values.username}
-          //className="form-control"
-          placeholder="Username"
-          className={
-              errors.username && touched.username
-                ? "form-control is-invalid"
-                : "form-control"
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            className="form-control"
+            placeholder="Email"
+            className={
+              errors.email && touched.email ? "form-control is-invalid" : "form-control"
             }
           />
+          {errors.email && touched.email ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.email}
+              </small>
+            ) : null}
         </div>
         <div className="form-group has-feedback">
           <input
-          type="text"
-          name="email"
-          onChange={handleChange}
-          value={values.email}
-          placeholder="Email"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            className="form-control"
+            placeholder="Password"
+            className={
+              errors.password && touched.password ? "form-control is-invalid" : "form-control"
+            }
           />
+          {errors.password && touched.password ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.password}
+              </small>
+            ) : null}
         </div>
         <div className="form-group has-feedback">
           <input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={values.password}
-          className="form-control"
-          placeholder="Password"
+            type="password"
+            name="confirm_password"
+            onChange={handleChange}
+            placeholder="Confirm Password"
+            className="form-control"
+            className={
+              errors.password && touched.password ? "form-control is-invalid" : "form-control"
+            }
           />
-        </div>
-        <div className="form-group has-feedback">
-          <input
-          type="password"
-          name="confirm-password"
-          onChange={handleChange}
-          placeholder="Confirm password"
-          />
+          {errors.confirm_password && touched.confirm_password ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.confirm_password}
+              </small>
+            ) : null}
         </div>
         <div className="row">
           <div className="col-md-12">
             <button
-            disabled={isSubmitting}
-            type="submit"
-            className="btn btn-primary btn-block btn-flat"
+              disabled={isSubmitting}
+              type="submit"
+              className="btn btn-primary btn-block btn-flat"
             >
-            Confirm</button>
+              Confirm
+            </button>
           </div>
         </div>
-      </form>
-  );
-}
-
+        </form>
+      );
+    };
   render() {
     return(
-    <div className="register-page">
-      <div className="register-box">
+<div className="register-box">
         <div className="register-logo">
           <a href="../../index2.html">
-            <b>Cangrejal de Fa</b> POS
+            <b>Basic</b>POS
           </a>
         </div>
         <div className="card">
           <div className="card-body register-card-body">
-            <p className="login-box-msg">Registrar un nuevo miembro</p>
-
+            <p className="login-box-msg">Register a new membership</p>
+ 
             <Formik
-              intialValues={{
-                fullname:"",
-                email:"",
-                password:"",
-                confirm_password:""
+              initialValues={{
+                fullname: "",
+                email: "",
+                password: "",
+                confirm_password: ""
               }}
-              onSubmit={(values, { setSubmitting }) => {
+              onSubmit={(values,{setSubmitting}) => {
                 console.log(values);
                 setSubmitting(false);
               }}
-              validationSchema={SignupSchema}>
-              {(props) => this.showForm(props)}
+              validationSchema={SignupSchema}
+            >
+              {props => this.showForm(props)}
             </Formik>
-            </div>
           </div>
+          {/* /.form-box */}
         </div>
+        {/* /.card */}
       </div>
     );
   }
