52d775997bf5dc6d387b255121dbc8264eba934b
Andres Torres <magoyde@gmail.com>
2021-11-10 01:40:54 -0500

    Implementando Login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,140 @@
 import React, { Component } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import swal from "sweetalert";
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "username is Too Short!")
+    .max(50, "username is Too Long!")
+    .required("username is Required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Email is Required"),
+  password: Yup.string().required("Password is required"),
+  confirm_password: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Both password need to be the same"
+  )
+});
 
 class Login extends Component {
+  submitForm = (values, history) => {
+    axios
+      .post("http://localhost:8080/login", values)
+      .then(response => {
+        console.log(response.data.result);
+        if (response.data.result === "success") {
+          localStorage.setItem("TOKEN_KEY", response.data.token);
+          swal("Todo bien!", response.data.message, "success")
+          .then(value =>{
+            history.push("/dashboard");
+          });
+        } else if(response.data.result === "error"){
+          swal("Error!", response.data.message, "error")
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        swal("Error!", "Something went wrong", "error");
+      });
+  };
+  showForm = ({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleSubmit,
+      setFieldValue,
+      isSubmitting
+    }) => {
+      return(
+        <form onSubmit={handleSubmit}>
+    
+        <div className="form-group has-feedback">
+          <input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            className="form-control"
+            placeholder="Email"
+            className={
+              errors.email && touched.email ? "form-control is-invalid" : "form-control"
+            }
+          />
+          {errors.email && touched.email ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.email}
+              </small>
+            ) : null}
+        </div>
+        <div className="form-group has-feedback">
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            className="form-control"
+            placeholder="Password"
+            className={
+              errors.password && touched.password ? "form-control is-invalid" : "form-control"
+            }
+          />
+          {errors.password && touched.password ? (
+              <small id="passwordHelp" class="text-danger">
+              {errors.password}
+              </small>
+            ) : null}
+        </div>
+       
+        <div className="row">
+          <div className="col-md-12">
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className="btn btn-primary btn-block btn-flat"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+        </form>
+      );
+    };
   render() {
-    return <div>Login</div>;
+    return(
+<div className="register-box mx-auto">
+        <div className="register-logo">
+          <a href="../../index2.html">
+            <b>Basic</b>POS
+          </a>
+        </div>
+        <div className="card">
+          <div className="card-body register-card-body">
+            <p className="login-box-msg">Register a new membership</p>
+ 
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              onSubmit={(values,{setSubmitting}) => {
+                this.submitForm(values, this.props.history);
+                console.log(values);
+                setSubmitting(false);
+              }}
+              validationSchema={SignupSchema}
+            >
+              {props => this.showForm(props)}
+            </Formik>
+          </div>
+          {/* /.form-box */}
+        </div>
+        {/* /.card */}
+      </div>
+    );
   }
 }
 
