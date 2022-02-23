12a9387e5411b918daca4569d0fc7522ce8438c4
Andres Torres <magoyde@gmail.com>
2022-02-23 07:53:07 -0500

    Cambios a front nueva orden

--- src/components/neworder/neworder.js
+++ src/components/neworder/neworder.js
@@ -4,7 +4,26 @@ class Neworder extends Component {
   render() {
     return (
         <div className="grid">
+          <div className="grid-item">
+            <div className="card">
+              <div className="card-header">
+                <h3>New Order</h3>
+                <div className="card-header-actions">
+                <button className="btn btn-primary">OK</button>
+                </div>
+              </div>
+            </div>
 
+            <div className="card">
+              <div className="card-header">
+                <h3>New Order</h3>
+                <div className="card-header-actions">
+                <button className="btn btn-primary">OK</button>
+                </div>
+              </div>
+            </div>
+            
+          </div>
         </div>
     );
   }
