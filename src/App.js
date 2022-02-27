1a6d53dcb6f10e14b5904cc1bae40dcfb440a011
Andres Torres <magoyde@gmail.com>
2022-02-26 22:24:37 -0500

    cambios en dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header";
-import Sidebar from './components/sidebar/sidebar';
 import Footer from './components/footer';
 import Dashboard from './components/dashboard';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
@@ -9,7 +8,6 @@ import Register from './components/register/register';
 import Login from './components/login/login';
 import Neworder from './components/neworder/neworder';
 
-import {PanelMenu} from 'primereact/panelmenu';
 
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
@@ -19,12 +17,6 @@ import "primeicons/primeicons.css";                                //icons
 
 function App() {
 
-  const items = [
-    {
-      label: 'Inicio',
-      icon: 'pi pi-fw pi-home',
-    }
-  ];
 
   return (
     <Router>
@@ -32,11 +24,11 @@ function App() {
         <div>
           <Header/>
           {/* <Sidebar/> */}
-          <Route path="/dashboard" component={Dashboard} />
+          <Dashboard />
+          {/* <Route path="/dashboard" component={Dashboard} /> */}
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/neworder" component={Neworder} />
-          <PanelMenu model={items} style={{width:'300px'}}/>
           <Footer/>
         </div>
       </Switch>
